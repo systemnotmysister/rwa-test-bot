fc54e1401e6da626fc03459c583e02ed67fc4ac7
systemnotmysister <48369226+systemnotmysister@users.noreply.github.com>
2024-10-10 15:12:58 +0300

    main.tsx disable stric

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,9 @@ import App from './App';
 import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
 );
 
 export default App;
